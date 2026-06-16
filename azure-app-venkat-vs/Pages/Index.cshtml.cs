4c89b5b16fa0d543d73307d93404ba3ba78b0239
Dell <Dell@DESKTOP-JL05E6N>
2026-06-17 04:54:50 +1000

    Add project files.

---
+++ azure-app-venkat-vs/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_venkat_vs.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
